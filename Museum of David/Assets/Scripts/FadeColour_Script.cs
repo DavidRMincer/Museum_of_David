d4582ca1475a183b0c7b321ffc390e36ddcc8af5
DavidRMincer <david.r.mincer@live.com>
2020-05-04 12:02:08 +0100

    Main Menu FINISHED

---
+++ Museum of David/Assets/Scripts/FadeColour_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeColour_Script : MonoBehaviour
+{
+    public void FadeColour(Color newColour, float duration)
+    {
+        StartCoroutine(FadeColIEnum(newColour, duration));
+    }
+
+    private IEnumerator FadeColIEnum(Color newColour, float duration)
+    {
+        float counter = 0f;
+        Color prevColour = GetComponent<Image>().color;
+
+        do
+        {
+            counter = (counter + Time.deltaTime > duration) ? duration : counter + Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+
+            GetComponent<Image>().color = Color.Lerp(prevColour, newColour, counter / duration);
+        } while (counter < duration);
+    }
+}
